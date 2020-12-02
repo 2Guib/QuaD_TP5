3b176ec401073673b5d264eeda67de9f0085ac27
2Guib <maxime.deguibert@orange.fr>
2020-12-02 09:26:39 +0100

    implementation feature 1,2,3,4,5,6,7 + test

---
+++ bienvenue/bienvenue.py
@@ -0,0 +1,70 @@
+def message(noms):
+    if len(noms.strip()) == 0:
+        return "Hello, my friend."
+
+    message = ""
+
+    tab_nom = tab_noms_min(noms)
+    tab_nom_maj = tab_noms_maj(noms)
+
+    if len(tab_nom) != 0:
+        message += "Hello, "
+        if (len(tab_nom)==1):
+            message += tab_nom[0].title()+"."
+        else:
+            for i in range(len(tab_nom)):
+                if i == len(tab_nom) - 1:
+                    message = message[0:-2] + " and " + tab_nom[i].title()
+                else:
+                    message += tab_nom[i].title() + ", "
+            message += "."
+
+    if len(tab_nom_maj) != 0:
+        if len(tab_nom) == 0:
+            message += "HELLO, "
+        else:
+            message += " AND HELLO, "
+        if len(tab_nom_maj) == 1:
+            message += tab_nom_maj[0] + " !"
+        else:
+            for i in range(len(tab_nom_maj)):
+                if i == len(tab_nom_maj) -1:
+                    message = message[0:-2] + " AND " + tab_nom_maj[i]
+                else:
+                    message += tab_nom_maj[i] + ", "
+            message += " !"
+
+
+
+    return message
+
+
+def tab_noms_min(noms):
+    tab_nom = []
+    nom = ""
+    for i in range(len(noms)):
+        if noms[i] == ',':
+            if nom != nom.upper():
+                tab_nom.append(nom)
+            nom = ""
+        else:
+            nom += noms[i]
+    if nom != nom.upper():
+        tab_nom.append(nom)
+
+    return tab_nom
+
+
+def tab_noms_maj(noms):
+    tab_nom = []
+    nom = ""
+    for c in noms:
+        if c == ',':
+            if nom == nom.upper():
+                tab_nom.append(nom)
+            nom = ""
+        else:
+            nom += c
+    if nom == nom.upper():
+        tab_nom.append(nom)
+    return tab_nom
--- test/test_bienvenue.py
+++ test/test_bienvenue.py
@@ -1,1 +1,27 @@
 import bienvenue.bienvenue as b
+
+def test_message_nom_simple():
+    assert b.message("Bob")=="Hello, Bob."
+    assert b.message("michel") == "Hello, Michel."
+
+def test_message_vide():
+    assert b.message("") == "Hello, my friend."
+    assert b.message("   ") == "Hello, my friend."
+    assert b.message("  ") == "Hello, my friend."
+    assert b.message("  bob") != "Hello, my friend."
+
+def test_message_cris_nom_simple():
+    assert b.message("JERRY") == "HELLO, JERRY !"
+    assert b.message("MICHEL") == "HELLO, MICHEL !"
+
+def test_message_noms_liste():
+    assert b.message("Amy,bob") == "Hello, Amy and Bob."
+    assert b.message("Bob,Michel") == "Hello, Bob and Michel."
+    assert b.message("Bob,Michel,denis,jacky") == "Hello, Bob, Michel, Denis and Jacky."
+
+def test_message_liste_avec_cris():
+    assert b.message("Amy,BOB,jerry") == "Hello, Amy and Jerry. AND HELLO, BOB !"
+    assert b.message("MICHEL,Amy,BOB,jerry") == "Hello, Amy and Jerry. AND HELLO, MICHEL AND BOB !"
+
+
+
